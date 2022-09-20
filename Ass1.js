2bed044539949558aba99c79dc12ec6bd45ab6ee
Tim Fiss <thefissk@hotmail.com>
2022-09-19 20:33:32 -0600

    got instanced rendering working.

--- Ass1.js
+++ Ass1.js
@@ -1,19 +1,26 @@
+let perf = { logPerformance: true };
+perf.totalFrameTime = 0;
+perf.frameTimes = [];
+perf.lastFrame = Date.now();
+
 //Rendering context variables
+let canvas;
 let gl;
 let instanceVBO;
+let program;
 
 const emmitter = {
-  location: vec2(0.0, 0.0),
-  emmissionRate: 1,
+  location: [0.0, 0.0],
+  emmissionRate: 1400,
 };
 //The Default particle and its attributes
 const defaultParticleProperties = {
-  color: vec4(1.0, 1.0, 0.25, 1), //RGBA
+  color: [1.0, 1.0, 0.25, 1], //RGBA
   size: 100.0,
   speed: 5,
-  directionBias: vec2(1.0, 0.0), //no bias, going right (+X)
+  directionBias: [-1.0, 0.0], //no bias, going right (+X)
   Lifetime: 60,
-  Shape: 0.5,
+  Shape: 3,
 };
 
 //A list of all the particles
@@ -21,9 +28,6 @@ let particles = [];
 
 //Transformation Matrices
 let aspect;
-let defaultMV = new mat4();
-let p = new mat4();
-let mvLoc, projLoc, colorLoc, moveLoc;
 
 //the first 3 data points are vertex locations. the last point is whether that vertex is moved
 let points = [
@@ -42,55 +46,72 @@ let points = [
 //First time setup
 window.onload = () => {
   //setup WebGl
-  let canvas = document.getElementById("gl-canvas");
-  gl = canvas.getContext("webgl2");
+  canvas = document.getElementById("gl-canvas");
+  gl = canvas.getContext("webgl2", { antialias: false });
   if (!gl) {
     canvas.parentNode.innerHTML("Cannot get WebGL2 Rendering Context");
   }
-  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
   gl.enable(gl.DEPTH_TEST);
   gl.enable(gl.CULL_FACE);
   gl.clearColor(0, 0, 0, 1);
 
-  let program = initShaders(gl, "shader.vert", "shader.frag");
+  program = initShaders(gl, "shader.vert", "shader.frag");
   gl.useProgram(program);
 
+  resizeScreen();
+
   //Load the shape into the buffer
-  const vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  //const vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
   gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
-  program.vPosition = gl.getAttribLocation(program, "vPosition");
-  gl.vertexAttribPointer(program.vPosition, 3, gl.FLOAT, gl.FALSE, 16, 0);
-  gl.enableVertexAttribArray(program.vPosition);
-  program.vMove = gl.getAttribLocation(program, "vMove");
-  gl.vertexAttribPointer(program.vMove, 1, gl.FLOAT, gl.FALSE, 16, 12);
-  gl.enableVertexAttribArray(program.vMove);
+  //program.vPosition = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 16, 0);
+  gl.enableVertexAttribArray(0);
+  //program.vMove = gl.getAttribLocation(program, "vMove");
+  gl.vertexAttribPointer(1, 1, gl.FLOAT, gl.FALSE, 16, 12);
+  gl.enableVertexAttribArray(1);
 
   instanceVBO = gl.createBuffer();
-  // Get addresses of shader uniforms
-  projLoc = gl.getUniformLocation(program, "p");
-  mvLoc = gl.getUniformLocation(program, "mv");
-  colorLoc = gl.getUniformLocation(program, "vColor");
-  moveLoc = gl.getUniformLocation(program, "moveBy");
+
+  // Get addresses of shader uniform
+  let mvLoc = gl.getUniformLocation(program, "mv");
+
+  //declare the uniforms
+  let mv = new mat4();
+  let p = new mat4();
 
   //Set initial view
   let eye = vec3(0.0, 0.0, 10.0);
   let at = vec3(0.0, 0.0, 0.0);
   let up = vec3(0.0, 1.0, 0.0);
 
-  aspect = canvas.clientWidth / canvas.clientHeight;
-  defaultMV = lookAt(eye, at, up);
+  mv = lookAt(eye, at, up);
+  gl.uniformMatrix4fv(mvLoc, gl.FALSE, flatten(transpose(mv)));
 
-  //Set up projection matrix
-  p = perspective(90, aspect, 1.0, 200.0);
-  gl.uniformMatrix4fv(projLoc, gl.FALSE, flatten(transpose(p)));
   //DRAW BABY DRAW
   requestAnimationFrame(animate);
 };
 
 const animate = () => {
-  //requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
+  if (perf.logPerformance) {
+    perf.frameTimes.push(Date.now() - perf.lastFrame);
+    let sh = 0;
+    if (perf.frameTimes.length > 60) sh = perf.frameTimes.shift();
+    perf.totalFrameTime += perf.frameTimes[perf.frameTimes.length - 1] - sh;
+    console.log(
+      `fps: ${Math.round(60000 / perf.totalFrameTime)} Particles: ${
+        particles.length
+      }`
+    );
+    perf.lastFrame = Date.now();
+  }
   handleKeys();
+  resizeScreen();
+  updateParticles();
+  spawnNewParticle(emmitter.emmissionRate);
+  updateParticles();
+  spawnNewParticle(emmitter.emmissionRate);
   updateParticles();
   spawnNewParticle(emmitter.emmissionRate);
   render();
@@ -104,64 +125,109 @@ const render = () => {
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   //Buffer Layout
-  //Per instance 16*4 MV, 4*4 Color, 1*4 Shape
-  let buffer = [];
-
-  particles.map((p) => {
-    let mv = structuredClone(defaultMV);
-    //doing this translation in the GPU might be faster than in JS. Definitely if we remove the 3rd dimension
-    mv = mult(mv, translate(p.location[0], p.location[1], 0));
-    mv = mult(mv, scale(p.size, p.size, p.size));
-    buffer = buffer.concat(transpose(mv).flat(1));
-    buffer = buffer.concat(p.color);
-    buffer = buffer.concat(p.Shape);
+  //Per instance 2*4 translate 1*4 size 4*4 color 1*4 shape = 8 + 4 + 16 + 4 = 32
+  const buffer = [];
+  particles.forEach((p) => {
+    buffer.push(...p.location);
+    buffer.push(p.size);
+    buffer.push(...p.color);
+    buffer.push(p.Shape);
   });
-  console.log(flatten(buffer));
   gl.bindBuffer(gl.ARRAY_BUFFER, instanceVBO);
-  gl.bufferData(gl.ARRAY_BUFFER, flatten(buffer), gl.DYNAMIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(buffer), gl.DYNAMIC_DRAW);
+  //translate
+  gl.vertexAttribPointer(2, 2, gl.FLOAT, gl.FALSE, 32, 0);
+  gl.enableVertexAttribArray(2);
+  //size
+  gl.vertexAttribPointer(3, 1, gl.FLOAT, gl.FALSE, 32, 8);
+  gl.enableVertexAttribArray(3);
+  //color
+  gl.vertexAttribPointer(4, 4, gl.FLOAT, gl.FALSE, 32, 12);
+  gl.enableVertexAttribArray(4);
+  //moveBy
+  gl.vertexAttribPointer(5, 1, gl.FLOAT, gl.FALSE, 32, 28);
+  gl.enableVertexAttribArray(5);
 
-  gl.uniform4fv(colorLoc, defaultParticleProperties.color);
-  gl.uniform1f(moveLoc, defaultParticleProperties.Shape);
+  gl.vertexAttribDivisor(2, 1);
+  gl.vertexAttribDivisor(3, 1);
+  gl.vertexAttribDivisor(4, 1);
+  gl.vertexAttribDivisor(5, 1);
 
-  gl.uniformMatrix4fv(mvLoc, gl.FALSE, flatten(transpose(defaultMV)));
-  gl.drawArrays(gl.TRIANGLE_FAN, 0, points.length);
+  gl.drawArraysInstanced(gl.TRIANGLE_FAN, 0, points.length, particles.length);
 };
+
+// color: [1.0, 1.0, 0.25, 1], //RGBA
+// size: 100.0,
+// speed: 5,
+// directionBias: [-1.0, 0.0], //no bias, going right (+X)
+// Lifetime: 60,
+// Shape: 3,
 
 const spawnNewParticle = (numberToSpawn) => {
   for (let i = 0; i < numberToSpawn; i++) {
     //because cloning an object should be simple zzzzz
-    let newPart = structuredClone(defaultParticleProperties);
-    newPart.mv = structuredClone(defaultMV);
-    newPart.location = [...emmitter.location];
+    let NP = {};
+    NP.color = [...defaultParticleProperties.color];
+    NP.size = defaultParticleProperties.size;
+    NP.Lifetime = defaultParticleProperties.Lifetime;
+    NP.Shape = defaultParticleProperties.Shape;
+    NP.location = [...emmitter.location];
 
     //create the movement translation
     const directionWiggle = ((Math.random() - 0.5) * Math.PI) / 3;
     const Dsin = Math.sin(directionWiggle);
     const Dcos = Math.cos(directionWiggle);
-    newPart.velocity = [
-      (newPart.directionBias[0] * Dcos + newPart.directionBias[1] * Dsin) *
-        newPart.speed,
-      (newPart.directionBias[0] * Dsin - newPart.directionBias[1] * Dcos) *
-        newPart.speed,
+    NP.velocity = [
+      (defaultParticleProperties.directionBias[0] * Dcos +
+        defaultParticleProperties.directionBias[1] * Dsin) *
+        defaultParticleProperties.speed,
+      (defaultParticleProperties.directionBias[0] * Dsin -
+        defaultParticleProperties.directionBias[1] * Dcos) *
+        defaultParticleProperties.speed,
     ];
 
     //add it to the particles list
-    particles.push(newPart);
+    particles.push(NP);
   }
 };
 
 //Removes expired particles, then shifts them based on their speed
 const updateParticles = () => {
   if (particles.length === 0) return;
-  //Hopefully before the end I will be able to explore whether remapping
-  //particles less efficient than iterating over every element and slicing
-  //expired particles
   particles = particles.filter((p) => {
     //remove expired particles
     if (p.Lifetime <= 1) return false;
 
     p.Lifetime -= 1;
-    p.location = [p.location[0] + p.velocity[0], p.location[1] + p.velocity[1]];
+    p.location[0] += p.velocity[0];
+    p.location[1] += p.velocity[1];
     return true;
   });
+};
+
+const resizeScreen = () => {
+  // Lookup the size the browser is displaying the canvas in CSS pixels.
+  const displayWidth = canvas.clientWidth;
+  const displayHeight = canvas.clientHeight;
+
+  // Check if the canvas is not the same size.
+  const needResize =
+    canvas.width !== displayWidth || canvas.height !== displayHeight;
+
+  if (needResize) {
+    // Make the canvas the same size
+    canvas.width = displayWidth;
+    canvas.height = displayHeight;
+
+    //reset the viewport
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    //reset the perspective uniform
+    const projLoc = gl.getUniformLocation(program, "p");
+    aspect = canvas.clientWidth / canvas.clientHeight;
+    p = perspective(120, aspect, 1.0, 200.0);
+    gl.uniformMatrix4fv(projLoc, gl.FALSE, flatten(transpose(p)));
+  }
+
+  return needResize;
 };
--- shader.vert
+++ shader.vert
@@ -1,11 +1,15 @@
 #version 300 es
 
-in vec3 vPosition;
-in float vMove;
+layout (location = 0) in vec3 vPosition;
+layout (location = 1) in float vMove;
+layout (location = 2) in vec2 translate;
+layout (location = 3) in float size;
+layout (location = 4) in vec4 vColor;
+layout (location = 5) in float moveBy;
 
-uniform mat4 p,mv;
-uniform vec4 vColor;
-uniform float moveBy;
+
+uniform mat4 p;
+uniform mat4 mv;
 
 out vec4 color;
 
@@ -17,9 +21,11 @@ void main()
     //a more sensible solution (like sending a short) to work.
     vec3 pos = vPosition;
     if(vMove > 0.0) {
-        pos = pos*moveBy;
+        pos *= moveBy;
     }
-    vec4 mvPosition = mv*vec4(pos,1.0);
+    pos += vec3(translate,0.0);
+
+    vec4 mvPosition = mv * vec4(pos,1.0);
 
     //Apply projection and send out
     gl_Position = p*mvPosition;
